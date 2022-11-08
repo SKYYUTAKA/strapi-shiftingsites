03937ae4ebe97a0156d66fb7574f071b9ed7a572
skyyutaka <yutaka009@gmail.com>
2022-11-08 11:07:43 +0800

    update config files for aws 20221108_1107

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,14 @@
-const path = require('path');
+// path: ./strapi-shiftingsites/config/database.js
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi-database1"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "strapidatabase009"),
     },
     useNullAsDefault: true,
   },
