6efb65e3b3ded516cbe836b96881053e1fb24899
skyyutaka <yutaka009@gmail.com>
2022-11-09 22:10:54 +0800

    202211_2210

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi-shiftingsites-database1"),
+      database: env("DATABASE_NAME", "strapi-shiftingsites-database"),
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "strapidatabase009"),
     },
